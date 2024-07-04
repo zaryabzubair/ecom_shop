41df16794409641323f012e185e441a86b62977d
zaryabzubair <zaryabzubair.m@gmail.com>
2024-07-05 02:01:21 +0500

    fetch product data

--- store/views.py
+++ store/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from ecom_shop.settings import FIREBASE_CONFIG
+import pyrebase
+
+firebase = pyrebase.initialize_app(FIREBASE_CONFIG)
+db = firebase.database()
+storage = firebase.storage()
 
 
 def ProductList(request):
-    return render(request, 'store/product_list.html')
+    products = db.child('products').get().key()
+    return render(request, 'store/product_list.html', {'products': products})
 
 
 def AddProduct(request):
