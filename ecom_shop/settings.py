86db66e9772984ba49cb0b5c776e235821fcf552
zaryabzubair <zaryabzubair.m@gmail.com>
2024-07-04 09:15:51 +0500

    update file

--- ecom_shop/settings.py
+++ ecom_shop/settings.py
@@ -123,6 +123,10 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
+# Media files
+MEDIA_URL = '/images/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
